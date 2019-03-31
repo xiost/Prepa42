9f0eff24bb283aa6780176e79e17f09958126dbe
Benjamin-poisson <49104726+Benjamin-poisson@users.noreply.github.com>
2019-03-31 17:10:47 +0200

    Update main.c

--- 42/day 02/Ex 00/main.c
+++ 42/day 02/Ex 00/main.c
@@ -1,14 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void ft_print_alphabet(void);
-
-int main(int argc, char *argv[])
-{
-   ft_print_alphabet();
-
-}
-
 void ft_print_alphabet(void)
 {
     char lettre = '`';
@@ -18,7 +7,6 @@ void ft_print_alphabet(void)
         lettre++;
         ft_putchar(lettre);
     }
-    return 0;
 }
 void    ft_putchar(char c)
 {
